1700f9a85b6f65df8265284ae82308f42254b144
Bruno Lima <brunolimadevelopment@gmail.com>
2017-09-01 23:59:57 -0300

    otimizacao nas functions

--- js/script.js
+++ js/script.js
@@ -1,37 +1,41 @@
-//objeto cena
-var scene = new THREE.Scene();
-//objeto camera
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-//objeto renderizado
-var renderer = new THREE.WebGLRenderer();
-	renderer.setSize(window.innerWidth, window.innerHeight);
+//organizando as variaveis.
+var camera, scene, renderer;
+var geometry, material, mesh;
 
-	document.body.appendChild(renderer.domElement);
+//função que inicia o cenário. 
+function init() {
+	//objeto render
+	renderer = new THREE.CanvasRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+	//objeto camera
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	//define a posição de profundidade do cubo na tela.
+	camera.position.z = 3;
+	//objeto scene 
+	scene = new THREE.Scene();
+	//objeto cubo e definiçao das coordenadas.
+	geometry = new THREE.CubeGeometry( 1, 1, 1 );
+	//objeto material para colorir.
+	material = new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true, wireframeLinewidth: 2 } );
+	//objeto mesh para receber as cordenadas da geometry + material"cor".
+	mesh = new THREE.Mesh( geometry, material );
+	//será adicionada às coordenadas. isso faz com que a camera e o cubo estejam um dentro do outro.
+	scene.add( mesh );
 
-//objeto cubo e definiçao das coordenadas
-var geometry = new THREE.BoxGeometry(1,1,1);
-//objeto para colorir
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-//objeto para receber as cordenadas da geometry + material"cor".
-var cube = new THREE.Mesh( geometry,material );
-//será adicionada às coordenadas. isso faz com que a camera e o cubo estejam um dentro do outro.
-scene.add( cube );
+}
 
-//defini a posição de profundidade do cubo na tela.
-camera.position.z = 3; 
-
-
-
-var animate = function(){
+function animate() {
 	//realiza a animação.
-	requestAnimationFrame( animate );	
-	
+	requestAnimationFrame( animate );
 	//Isso será executado em cada quadro (60 vezes por segundo) e dará ao cubo uma boa animação de rotação. 
-	cube.rotation.x += 0.02;
-	cube.rotation.y += 0.02;
-
+	mesh.rotation.x = Date.now() * 0.0005;
+	mesh.rotation.y = Date.now() * 0.001;
+	
 	//renderiza a scene e camera junto com a animação do cubo.
-	renderer.render(scene, camera);
-};
+	renderer.render( scene, camera );
 
+}
+
+init();
 animate();
